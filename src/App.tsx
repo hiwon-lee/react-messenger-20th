b5cec87f7a52c04cfc0770bf72810ba8595f8279
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-09-26 10:36:55 +0900

    feat : initial chat main

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import Chat from './pages/Chat/Chat';
+import './App.css';
+import StatusBar from './components/common/StatusBar';
+
 function App() {
   return (
-    <div>
-      <h1>20기 프론트엔드 파이팅!!! 디자인과 사이좋게 지내요~~~</h1>
-    </div>
+    <>
+      <div className="App">
+        <StatusBar />
+        <Chat />
+      </div>
+    </>
   );
 }
 
