dab9d0771c3950fca15ac19cfd3b9e1075c776ab
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-09-26 21:45:57 +0900

    docs : 주석 삭제

--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,29 +1,3 @@
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const initialState = {
-//   _id: '',
-//   name: '',
-//   profile_img: '',
-// };
-
-// export const userSlice = createSlice({
-//   name: 'user',
-//   initialState,
-//   reducers: {
-//     setUser: (state, action) => {
-//       state._id = action.payload._id;
-//       state.name = action.payload.name;
-//       state.profile_img = action.payload.profile_img;
-//     },
-//   },
-// });
-
-// // Action creators are generated for each case reducer function
-// export const { setUser, setToken, logout, setOnlineUser, setSocketConnection } =
-//   userSlice.actions;
-
-// export default userSlice.reducer;
-
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
