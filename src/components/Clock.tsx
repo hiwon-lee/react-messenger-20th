97c14015e5d8a6d57d9e2c994c0e1b0ace8f62f0
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-09-26 10:37:48 +0900

    feat : component

---
+++ src/components/Clock.tsx
@@ -0,0 +1,31 @@
+import { useRef, useEffect } from 'react';
+
+// Clock : 상단 바 시간
+
+export default function Clock() {
+  const timeRef = useRef<any>(null); // 일단 any써뒀는데, 아닌 것 같음
+
+  useEffect(() => {
+    const updateClock = () => {
+      if (timeRef.current) {
+        const hour = new Date().getHours();
+        const minute = new Date().getMinutes();
+
+        // 00:00 format
+        const formattedTime = `${String(hour).padStart(2, '0')}:${String(
+          minute
+        ).padStart(2, '0')}`;
+        timeRef.current.textContent = formattedTime;
+      }
+    };
+
+    // 초기에 시계 세팅
+    updateClock();
+
+    const id = setInterval(updateClock, 60000); // 1분에 한 번 씩 => 오차 있을 듯
+
+    return () => clearInterval(id);
+  }, []);
+
+  return <p ref={timeRef}></p>;
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return (
+    <div
+      className="rounded-full w-7 h-7
+ bg-pink-light text-pink-dark flex items-center justify-center 
+"
+    >
+      박
+    </div>
+  );
+}
