2c0e24df297c533725c8f8a46c1aa3b4ece756c7
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-10-22 22:00:35 +0900

    style : add search ui

--- src/pages/Members/index.tsx
+++ src/pages/Members/index.tsx
@@ -6,11 +6,27 @@ import defaultMessages from '../../data/messages.json';
 import Header from '@components/common/Header';
 import BottomMenu from '@components/common/BottomMenu';
 import MemberListItem from '@components/MemberListItem';
+import Form from '@components/Form';
 
 export default function Members() {
+  const addMember = (text: string) => {
+    alert(text);
+  };
   return (
     <div className="h-full flex flex-col">
       <Header title="목록" />
+      <div className="flex p-2 gap-2">
+        <Form
+          placeHolder="검색"
+          onSubmit={(text: string) => addMember(text)}
+        />
+      </div>
+      <MemberListItem
+        _id="temp"
+        name="이희원"
+        lastMessage="꺼져"
+        lastTimeStamp="오후 10:34"
+      />
       <MemberListItem
         _id="temp"
         name="이희원"
