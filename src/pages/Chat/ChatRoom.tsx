d86117d47300437d52008bd3b8c75da7c722cecd
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-11-02 16:36:01 +0900

    feat : userID기반 채팅데이터 chatRoom에 넘김

--- src/pages/Chat/ChatRoom.tsx
+++ src/pages/Chat/ChatRoom.tsx
@@ -2,50 +2,76 @@ import { useSelector } from 'react-redux';
 import { RootState } from '@redux/store';
 import { useLocalStorage } from '@hooks/useLocalStorage';
 
-import defaultMessages from '../../data/messages_1.json';
-import { ChatInterface, MessageInterface } from '@interface/ChatInterface';
+import defaultUserData from '@data/users.json';
+import { ChatData, MessageInterface } from '@interface/ChatInterface';
 import ChatHeader from './ChatHeader';
 import ChatMain from './ChatMain';
 import ChatForm from './ChatForm';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { UserInterface } from '@interface/UserInterface';
+import useChatMessages from '@hooks/useChatMessages';
 
 export default function ChatRoom() {
+  const { chatData, addMessage } = useChatMessages();
+  const userName = useSelector((state: RootState) => state.user.name);
   // URL에서 userId 가져오기
   const { userId } = useParams<{ userId: string }>();
-
-  const userName = useSelector((state: RootState) => state.user.name);
-  const initialMessages = defaultMessages as ChatInterface;
-
-  const [savedMessage, setMessages] = useLocalStorage<ChatInterface>({
-    key: 'chat_' + initialMessages.id,
-    initialValue: initialMessages,
+  // TODO : param으로 넘어온 id를 가지고 localstorage에 있는 사용자 정보 가져오기
+  const [members, setMembers] = useLocalStorage<UserInterface[]>({
+    key: 'members',
+    initialValue: defaultUserData,
   });
-  useEffect(() => {
-    console.log(savedMessage);
-  });
+  //   const friendName = members.find((member) => member.id === userId)?.userName;
 
-  // addMessage : 메시지 저장 로직 추가 -> localStorage에 차곡히 쌓음
-  const addMessage = (text: string) => {
-    const now = new Date();
-    const sender = userName ? 'you' : 'me'; // TODO : 누가 보낸건지,, 로직 수정
-    const messageData: MessageInterface = {
-      sender: sender,
+  const friendName: UserInterface | undefined = members.find(
+    (member) => member.id == userId
+  );
+  const [friendMessages, setFriendMessages] = useState<MessageInterface[]>([]);
+
+  // TODO : 사용자에 해당하는 채팅메시지들 가져와
+
+  // TODO : 화면에 렌더링
+  // 새로운 메시지를 추가하는 함수
+  const handleAddMessage = (text: string) => {
+    if (!userId) return;
+
+    const newMessage = {
+      sender: userName || 'me',
       message: text,
-      timeStamp: now.toLocaleTimeString('ko-KR', {
+      timeStamp: new Date().toLocaleTimeString('ko-KR', {
         hour: '2-digit',
         minute: '2-digit',
-      }), // HH:MM
-      date: now.toISOString().split('T')[0], // YYYY-MM-DD
-      emoji: undefined, // 이모지 없음
+      }),
+      date: new Date().toISOString().split('T')[0],
+      emoji: undefined,
     };
+
+    addMessage(userId, newMessage);
   };
+
+  // TODO : 메시지 던지면 로컬스토리지에 업데이트
+
+  useEffect(() => {
+    console.log(chatData);
+    if (userId && chatData[userId]) {
+      setFriendMessages(chatData[userId]);
+      console.log(friendMessages);
+    } else {
+      console.log('userId에 해당하는 값이 없습니다.');
+    }
+  });
+
   return (
     <div className="h-full flex flex-col">
       <ChatHeader />
       방이다.
-      {/* <ChatMain messages={messages} /> */}
-      <ChatForm addMessage={(text: string) => addMessage(text)} />
+      <ChatMain
+        id={userId || ''}
+        sender={userName || ''}
+        messages={friendMessages}
+      />
+      <ChatForm addMessage={handleAddMessage} />
     </div>
   );
 }
