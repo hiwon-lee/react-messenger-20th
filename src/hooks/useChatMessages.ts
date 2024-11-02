dccc6bf19cefd8403fc46a3ab6529ffd6dfcfc6a
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-11-02 12:42:02 +0900

    feat : uesrID기반 대화데이터 로컬스토리지에 저장

---
+++ src/hooks/useChatMessages.ts
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react';
+import userDataList from '@data/users.json';
+import { MessageInterface } from '@interface/ChatInterface';
+import { UserInterface } from '@interface/UserInterface';
+
+interface ChatData {
+  [userId: string]: MessageInterface[];
+}
+
+function useChatMessages() {
+  const [chatData, setChatData] = useState<ChatData>({});
+
+  // 로컬 스토리지에서 메시지 로드
+  useEffect(() => {
+    const savedData = localStorage.getItem('chatData');
+    console.log(getUserDataById());
+    if (savedData) {
+      setChatData(JSON.parse(savedData));
+    } else {
+      initializeChatData();
+    }
+  }, []);
+
+  // users.json에 있는 사용자들의 ID정보를 배열로 가져오는 함수
+  const getUserDataById = () => {
+    const userData: UserInterface[] = userDataList;
+    const usersIdArr: string[] = userData.map((data) => data.id);
+    return usersIdArr;
+  };
+
+  // 초기화: 처음 사용할 때 JSON 데이터를 로컬 스토리지에 저장
+  const initializeChatData = () => {
+    // JSON 데이터 로드 (서버로부터 초기 데이터를 불러오는 형태도 가능)
+    const userIdArr: string[] = getUserDataById();
+    const allUserMessages: ChatData = {};
+    userIdArr.forEach((userID, idx) => {
+      import(`@data/messages_${userID}.json`).then((messagesData) => {
+        // 새로운 배열을 생성하고 첫 번째 메시지를 추가
+        console.log(`userID ${userID}`);
+        console.log(messagesData);
+        allUserMessages[userID] = messagesData.messages;
+      });
+    });
+    localStorage.setItem('messageData', JSON.stringify(allUserMessages));
+    setChatData(allUserMessages);
+    // import('@data/messages_1.json').then((messages_1) => {
+    //   import('@data/messages_2.json').then((messages_2) => {
+    //     import('@data/messages_3.json').then((messages_3) => {
+    //       const initialData: ChatData = {
+    //         '1': messages_1.messages,
+    //         '2': messages_2.messages,
+    //         '3': messages_3.messages,
+    //       };
+    //       localStorage.setItem('chatData', JSON.stringify(initialData));
+    //       setChatData(initialData);
+    //     });
+    //   });
+    // });
+  };
+
+  // 새로운 메시지 추가 함수
+  const addMessage = (userId: string, newMessage: MessageInterface) => {
+    const updatedMessages = chatData[userId]
+      ? [...chatData[userId], newMessage]
+      : [newMessage];
+    const updatedChatData = { ...chatData, [userId]: updatedMessages };
+    setChatData(updatedChatData);
+    localStorage.setItem('chatData', JSON.stringify(updatedChatData));
+  };
+
+  // 마지막 메시지 미리보기 함수
+  const getLastMessagePreview = (userId: string) => {
+    const messages = chatData[userId] || [];
+    if (messages.length === 0) return { message: '', timeStamp: '' };
+    const lastMessage = messages[messages.length - 1];
+    return {
+      message: lastMessage.message,
+      timeStamp: lastMessage.timeStamp,
+    };
+  };
+
+  return { chatData, addMessage, getLastMessagePreview };
+}
+
+export default useChatMessages;
--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,13 +1,9 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '@redux/store';
 import { useLocalStorage } from '@hooks/useLocalStorage';
+import useChatMessages from '@hooks/useChatMessages';
 
 import userList from '../../data/users.json';
-import { ChatInterface, MessageInterface } from '@interface/ChatInterface';
-import ChatHeader from './ChatHeader';
-import ChatMain from './ChatMain';
-import ChatForm from './ChatForm';
-import { useEffect } from 'react';
 import { MemberListItem } from '@components/MemberListItem';
 import Header from '@components/common/Header';
 import { MainButton } from '@components/Button';
@@ -15,21 +11,34 @@ import BottomMenu from '@components/common/BottomMenu';
 import Main from '@components/common/MainContent';
 import StatusBar from '@components/common/StatusBar';
 import Form from '@components/Form';
+import { useEffect } from 'react';
 
 export default function Chat() {
   const userName = useSelector((state: RootState) => state.user.name);
+  const { chatData, addMessage, getLastMessagePreview } = useChatMessages();
 
   // TODO : hook 분리하기
   const searchMember = (text: string) => {
     alert(text);
   };
 
+  // TODO : 사용자 id를 가져와
+  // TODO : 사용자 id와 일치하는 message_{id}.json을 가져와
+  // TODO : 각 json파일을 배열에 담고
+  // TODO : map함수를 활용해서 채팅상대이름, 최근채팅미리보기, 채팅상대 프로필이미지 가 담긴 아이템을 나열해서 리스트로 완성
+  // TODO : ~를 통합한 custom hook 구현
+  useEffect(() => {
+    console.log('chat');
+    console.log(chatData);
+  });
+
+  // TODO :
   return (
     <div className="h-full flex flex-col">
       <StatusBar />
       <Header title="채팅" />
       <Main>
-        <div className="flex gap-4 justify-between h-23 mx-5 my-3  bg-border-l-rose-700 ">
+        <div className="flex gap-4 justify-between h-23 mx-5 my-3 bg-border-l-rose-700 ">
           <Form
             key="searchForm"
             onSubmit={(text: string) => searchMember(text)}
@@ -42,6 +51,18 @@ export default function Chat() {
             children="박"
           />
         </div>
+        {Object.keys(chatData).map((userId) => {
+          const lastMessageInfo = getLastMessagePreview(userId);
+          return (
+            <MemberListItem
+              key={userId}
+              _id={userId}
+              name={`${userId}`}
+              lastMessage={lastMessageInfo.message}
+              lastTimeStamp={lastMessageInfo.timeStamp}
+            />
+          );
+        })}
         {userList.map((user, idx) => (
           <MemberListItem
             key={user.id}
